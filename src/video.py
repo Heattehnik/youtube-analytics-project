2536ef39a736192e4417e959d0c30223d6fb2d51
roman <gadjka@mail.ru>
2023-04-18 11:26:29 +0500

    solved

--- src/video.py
+++ src/video.py
@@ -13,10 +13,21 @@ class Video:
         self.video_id = video_id
         self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                           id=self.video_id).execute()
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
+        self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
 
-    def show(self):
-        return self.video_response
+    def __str__(self):
+        return f'{self.video_title}'
 
-if __name__ == '__main__':
-    video = Video('9lO06Zxhu88')
-    print(video.show())
+
+class PLVideo(Video):
+
+    def __init__(self, video_id, pl_id):
+        self.pl_id = pl_id
+        super().__init__(video_id)
+
+
+
+
