65750b24fd8b568121b62b07d9e4c7d0a9cc1811
roman <gadjka@mail.ru>
2023-04-26 13:56:02 +0500

    solved

--- src/video.py
+++ src/video.py
@@ -13,13 +13,22 @@ class Video:
         self.video_id = video_id
         self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                           id=self.video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
-        self.views_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.likes_count = self.video_response['items'][0]['statistics']['likeCount']
+        self.title = None
+        self.url = None
+        self.views_count = None
+        self.like_count = None
+        try:
+            self.title = self.video_response['items'][0]['snippet']['title']
+        except IndexError:
+            print('Video_id некорректен')
+        else:
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = self.video_response['items'][0]['snippet']['thumbnails']['default']['url']
+            self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
 
 
 class PLVideo(Video):
